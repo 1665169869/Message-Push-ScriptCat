f232ad3a73aa9e917a07751d2bbe802a9aa3b822
白羽 <1665169869@qq.com>
2023-06-12 23:10:20 +0800

    新增tamplate默认模板,后续可能会修改

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,30 @@
  * @Author: 白羽
  * @Date: 2023-06-05 20:47:21
  * @LastEditors: 白羽
- * @LastEditTime: 2023-06-07 01:02:55
+ * @LastEditTime: 2023-06-12 22:31:21
  * @FilePath: \scriptcat-push-weixin\webpack.config.js
  * @Description: 
  */
 const path = require('path');
 const ScriptCatWebpackPlugin = require('scriptcat-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const USERCONFIG = {
+    "信息配置": {
+        accessKey: {
+            title: "accessKey",
+            type: "text",
+            description: "sciprtpush accessKey"
+        },
+        "推送标题": {
+            title: "推送标题",
+            type: "text",
+            description: "推送的标题 不支持模板字符串",
+            default: "宝宝的专属推送"
+        }
+    }
+}
+
 module.exports = {
     mode: "development",
     entry: "@/main.js",
@@ -28,13 +45,14 @@ module.exports = {
                 crontab: "* 10-23 once * *",
                 grant: [
                     "GM_xmlhttpRequest",
-                    "GM_log"
+                    "GM_log",
+                    "GM_getValue"
                 ],
                 require: [
                     "https://scriptcat.org/lib/946/%5E1.0.1/PushCat.js"
                 ]
             }
-        }),
+        }, USERCONFIG),
         new CleanWebpackPlugin()
     ],
     module: {
